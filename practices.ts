88715c990e9f0ca42276afba0603fa941919382e
Orlando Lujan <orlandoperezlujan@gmail.com>
2024-07-02 22:09:10 -0600

    openOrSenior function

--- practices.ts
+++ practices.ts
@@ -377,4 +377,62 @@ function isPangram(phrase: string): boolean {
 }
 
 //console.log(isPangram("The quick brown fox jumps over the lazy dog."));
-console.log(isPangram("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ"));
+//console.log(isPangram("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ"));
+
+/* 
+Categorize New Member
+
+DESCRIPTION:
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+*/
+
+function openOrSenior(data: number[][]): string[] {
+  let member: string[] = [];
+
+  for (let datas of data) {
+    datas[0] >= 55 && datas[1] > 7
+      ? member.push("Senior")
+      : member.push("Open");
+  }
+
+  return member;
+}
+
+// console.log(
+//   openOrSenior([
+//     [45, 12],
+//     [55, 21],
+//     [19, -2],
+//     [104, 20],
+//   ])
+// );
+
+// console.log(
+//   openOrSenior([
+//     [3, 12],
+//     [55, 1],
+//     [91, -2],
+//     [53, 23],
+//   ])
+// );
+
+// console.log(
+//   openOrSenior([
+//     [59, 12],
+//     [55, -1],
+//     [12, -2],
+//     [12, 12],
+//   ])
+// );
